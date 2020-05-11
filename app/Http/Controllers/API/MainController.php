1c640c55902c24941e86451fda2da1b5a0a1decc
Edgardo Castro <edgardocastro728@gmail.com>
2020-05-11 13:33:01 +0000

    Updating API list and README.md

--- app/Http/Controllers/API/MainController.php
+++ app/Http/Controllers/API/MainController.php
@@ -15,18 +15,20 @@ class MainController extends BaseController
     public function index()
     {
         $list = [
-            'Paged Courses List' => 'https://35.247.238.31/apicourses',
-            'Full Courses List' => 'https://35.247.238.31/apicourses/all',
-            'Show Course' => 'https://35.247.238.31/apicourses/{id}',
-            'Create Course' => 'https://35.247.238.31/apicourses',
-            'Edit Course' => 'https://35.247.238.31/apicourses/{id}',
-            'Delete Course' => 'https://35.247.238.31/apicourses/{id}',
-            'Paged Students List' => 'https://35.247.238.31/apistudents',
-            'Full Students List' => 'https://35.247.238.31/apistudents/all',
-            'Show Student' => 'https://35.247.238.31/apistudents/{id}',
-            'Create Student' => 'https://35.247.238.31/apistudents',
-            'Edit Student' => 'https://35.247.238.31/apistudents/{id}',
-            'Delete Student' => 'https://35.247.238.31/apistudents/{id}'
+            'API List' => 'https://35.247.238.31/api/',  
+            'Token for Auth' => 'https://35.247.238.31/api/token',            
+            'Paged Courses List' => 'https://35.247.238.31/api/courses',
+            'Full Courses List' => 'https://35.247.238.31/api/courses/all',
+            'Show Course' => 'https://35.247.238.31/api/courses/{id}',
+            'Create Course' => 'https://35.247.238.31/api/courses',
+            'Edit Course' => 'https://35.247.238.31/api/courses/{id}',
+            'Delete Course' => 'https://35.247.238.31/api/courses/{id}',
+            'Paged Students List' => 'https://35.247.238.31/api/students',
+            'Full Students List' => 'https://35.247.238.31/api/students/all',
+            'Show Student' => 'https://35.247.238.31/api/students/{id}',
+            'Create Student' => 'https://35.247.238.31/api/students',
+            'Edit Student' => 'https://35.247.238.31/api/students/{id}',
+            'Delete Student' => 'https://35.247.238.31/api/students/{id}'
         ];
             
         return $this->sendResponse($list, 'Available API routes List.');
